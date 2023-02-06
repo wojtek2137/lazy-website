d9f6b9cb479890aa071086baf7d85632f52a4ea0
wojtek2137 <adebe.beats@gmail.com>
2023-02-06 18:54:40 +0100

    fix vh

--- src/ui/pages/MainView/MainView.tsx
+++ src/ui/pages/MainView/MainView.tsx
@@ -1,29 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { ContentWrapper, ExtraSmallHeader, Header, HeaderWrapper, Logo, MainContainer, SubHeader, VideoWrapper } from './MainView.style';
 import dragonVideo2022 from 'src/assets/video/dragon2022.mp4';
-import styled from '@emotion/styled';
-import { colors } from 'src/config/theme';
 
-export const MuteButton = styled('a')`
-   color: ${colors.primary.sandy};
-   cursor: pointer;
-   font-size: 20px;
-   margin: 20px;
-   text-align: center;
-   text-transform: uppercase;
-   &:hover{
-   color: ${colors.primary.white};
+// export const MuteButton = styled('a')`
+//    color: ${colors.primary.sandy};
+//    cursor: pointer;
+//    font-size: 20px;
+//    margin: 20px;
+//    text-align: center;
+//    text-transform: uppercase;
+//    &:hover{
+//    color: ${colors.primary.white};
 
-   }
-`;
+//    }
+// `;
 
 export function MainView() {
-    const [isMuted, setIsMuted] = useState<boolean>(true);
+    // const [isMuted, setIsMuted] = useState<boolean>(true);
 
     return (
         <>
             <MainContainer>
-                <VideoWrapper src={dragonVideo2022} autoPlay loop muted={isMuted} playsInline></VideoWrapper>
+                <VideoWrapper src={dragonVideo2022} autoPlay loop muted={true} playsInline></VideoWrapper>
                 <ContentWrapper>
                     <Logo />
                     <HeaderWrapper>
@@ -31,7 +29,6 @@ export function MainView() {
 
                         <Header>associated with swing dancing </Header>
                         <SubHeader>and not only</SubHeader>
-                        <MuteButton onClick={() => setIsMuted(!isMuted)}>{isMuted ? 'Click to Unmute music' : 'Mute'}</MuteButton>
 
                         <ExtraSmallHeader>
                             Our website is under reconstruction see you soon...
