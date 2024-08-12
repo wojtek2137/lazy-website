f05e2dc6d8d124c72d265cbf260e771216cef408
wojtek2137 <adebe.beats@gmail.com>
2024-08-12 10:18:13 +0200

    change laanguage to polish

--- src/ui/pages/MainView/MainView.tsx
+++ src/ui/pages/MainView/MainView.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
-import { ContentWrapper, Header, HeaderWrapper, Logo, MainContainer, SubHeader, VideoWrapper } from './MainView.style';
-import dragonVideo2022 from 'src/assets/video/dragon2022.mp4';
+import React from "react";
+import {
+  ContentWrapper,
+  Header,
+  HeaderWrapper,
+  Logo,
+  MainContainer,
+  SubHeader,
+  VideoWrapper,
+} from "./MainView.style";
+import dragonVideo2022 from "src/assets/video/dragon2022.mp4";
 
 // export const MuteButton = styled('a')`
 //    color: ${colors.primary.sandy};
@@ -16,28 +24,34 @@ import dragonVideo2022 from 'src/assets/video/dragon2022.mp4';
 // `;
 
 export function MainView() {
-    // const [isMuted, setIsMuted] = useState<boolean>(true);
+  // const [isMuted, setIsMuted] = useState<boolean>(true);
 
-    return (
-        <>
-            <MainContainer>
-                <VideoWrapper src={dragonVideo2022} autoPlay loop muted={true} playsInline></VideoWrapper>
-                <ContentWrapper>
-                    <Logo />
-                    <HeaderWrapper>
-                        <SubHeader>originated from Krakow Poland</SubHeader>
+  return (
+    <>
+      <MainContainer>
+        <VideoWrapper
+          src={dragonVideo2022}
+          autoPlay
+          loop
+          muted={true}
+          playsInline
+        ></VideoWrapper>
+        <ContentWrapper>
+          <Logo />
+          <HeaderWrapper>
+            {/* <SubHeader>originated from Krakow Poland</SubHeader> */}
+            <SubHeader>zespół zawodowych muzyków jazzowych z Krakowa</SubHeader>
 
-                        <Header>associated with swing dancing </Header>
-                        <SubHeader>and not only</SubHeader>
+            <Header>Swingowa muzyka do tańca </Header>
+            {/* <Header>associated with swing dancing </Header> */}
+            <SubHeader>i nie tylko</SubHeader>
 
-                        {/* <ExtraSmallHeader>
+            {/* <ExtraSmallHeader>
                             Our website is under reconstruction see you soon...
                         </ExtraSmallHeader> */}
-
-                    </HeaderWrapper>
-
-                </ContentWrapper>
-            </MainContainer>
-        </>
-    );
-};
+          </HeaderWrapper>
+        </ContentWrapper>
+      </MainContainer>
+    </>
+  );
+}
