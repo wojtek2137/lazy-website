568f9de5359ed9c6b888d16ebcc73c2812cc4c83
wojtek2137 <adebe.beats@gmail.com>
2024-08-12 10:05:50 +0200

    change contact view and fix src albums errros

--- src/ui/pages/ListenUsView/ListenUsView.tsx
+++ src/ui/pages/ListenUsView/ListenUsView.tsx
@@ -1,102 +1,106 @@
-import React from 'react';
-import { Card } from 'src/ui/components/Card';
-import { ListenAlbumsWrapper, ListenUsHeader, ListenUsSection } from './ListenUsView.style';
-import LazySwingersCover from 'src/assets/images/lazy.png';
-import LazyChristmasCover from 'src/assets/images/christmas.jpg';
-import PolishSwingCover from 'src/assets/images/polish.jpg';
-import TagSpotify from 'src/assets/images/spotify.png';
-import TagTidal from 'src/assets/images/tidal.png';
-import TagYouTube from 'src/assets/images/youtube.png';
-import TagAppleMusic from 'src/assets/images/apple.png';
+import React from "react";
+import { Card } from "src/ui/components/Card";
+import {
+  ListenAlbumsWrapper,
+  ListenUsHeader,
+  ListenUsSection,
+} from "./ListenUsView.style";
+import LazySwingersCover from "src/assets/images/lazy.png";
+import LazyChristmasCover from "src/assets/images/christmas.jpg";
+import PolishSwingCover from "src/assets/images/polish.jpg";
+import TagSpotify from "src/assets/images/spotify.png";
+import TagTidal from "src/assets/images/tidal.png";
+import TagYouTube from "src/assets/images/youtube.png";
+import TagAppleMusic from "src/assets/images/apple.png";
 export interface CardConfigType {
-    name: string,
-    cover: string,
-    data: Array<{
-        src: string,
-        imageUrl: string
-    }>
+  name: string;
+  cover: string;
+  data: Array<{
+    src: string;
+    imageUrl: string;
+  }>;
 }
 
 const cards: Array<CardConfigType> = [
-    {
-        name: 'Lazy Swingers Band',
-        cover: LazySwingersCover,
-        data: [
-            {
-                src: 'https://open.spotify.com/artist/6zZ4pSM6Omz7z6T97DIg78',
-                imageUrl: TagSpotify
-            },
-            {
-                src: 'https://tidal.com/browse/album/136916865',
-                imageUrl: TagTidal
-            },
-            {
-                src: 'https://www.youtube.com/watch?v=7vk4D1ikMAI&list=OLAK5uy_mBdrWLxeD30Betmkn8hfJS_eraYm5CVEs',
-                imageUrl: TagYouTube
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagAppleMusic
-            },
-        ]
-    },
-    {
-        name: 'Lazy Christmas',
-        cover: LazyChristmasCover,
-        data: [
-            {
-                src: 'https://open.spotify.com/album/5XkBiCPt7nEQUnGjLSjG3P',
-                imageUrl: TagSpotify
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagTidal
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagYouTube
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagAppleMusic
-            },
-        ]
-    },
-    {
-        name: 'Polish Swing For Lindy Hop',
-        cover: PolishSwingCover,
-        data: [
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagSpotify
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagTidal
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagYouTube
-            },
-            {
-                src: 'https://music.apple.com/pl/album/lazy-swingers-band/1507094483',
-                imageUrl: TagAppleMusic
-            },
-        ]
-    },
+  {
+    name: "Lazy Swingers Band",
+    cover: LazySwingersCover,
+    data: [
+      {
+        src: "https://open.spotify.com/album/6eKPp0p7fb6w0SQlyvHzp0",
+        imageUrl: TagSpotify,
+      },
+      {
+        src: "https://tidal.com/browse/album/136916865",
+        imageUrl: TagTidal,
+      },
+      {
+        src: "https://music.youtube.com/playlist?list=OLAK5uy_mZWYpsOls5ank7HnSNuvOeR3eQgvZO9t8",
+        imageUrl: TagYouTube,
+      },
+      {
+        src: "https://music.apple.com/pl/album/lazy-swingers-band/1507094483",
+        imageUrl: TagAppleMusic,
+      },
+    ],
+  },
+  {
+    name: "Lazy Christmas",
+    cover: LazyChristmasCover,
+    data: [
+      {
+        src: "https://open.spotify.com/album/5XkBiCPt7nEQUnGjLSjG3P",
+        imageUrl: TagSpotify,
+      },
+      {
+        src: "https://tidal.com/browse/album/168182006",
+        imageUrl: TagTidal,
+      },
+      {
+        src: "https://music.youtube.com/playlist?list=OLAK5uy_lkk1xPxBViBc6BJV15Z1go2wJyJRYvNQI",
+        imageUrl: TagYouTube,
+      },
+      {
+        src: "https://music.apple.com/pl/album/lazy-christmas-single/1547365804",
+        imageUrl: TagAppleMusic,
+      },
+    ],
+  },
+  {
+    name: "Polish Swing For Lindy Hop",
+    cover: PolishSwingCover,
+    data: [
+      {
+        src: "https://open.spotify.com/album/66XnsvDO5h60grrk1hpzkG",
+        imageUrl: TagSpotify,
+      },
+      {
+        src: "https://tidal.com/browse/album/236091664",
+        imageUrl: TagTidal,
+      },
+      {
+        src: "https://music.youtube.com/playlist?list=OLAK5uy_n6alLLIMeF5-DwTYv1UvoAMrzi1GeIaBY",
+        imageUrl: TagYouTube,
+      },
+      {
+        src: "https://music.apple.com/pl/album/polish-swing-for-lindy-hop/1632523593",
+        imageUrl: TagAppleMusic,
+      },
+    ],
+  },
 ];
 
 export function ListenUsView() {
-    return (
-        <ListenUsSection id="albums">
-            <ListenUsHeader>
-                Albums
-            </ListenUsHeader>
-            <ListenAlbumsWrapper>
-                {cards.map(card => {
-                    return <Card name={card.name} coverSrc={card.cover} tags={card.data} />
-                })}
-            </ListenAlbumsWrapper>
-        </ListenUsSection>
-    );
-};
+  return (
+    <ListenUsSection id="albums">
+      <ListenUsHeader>Albums</ListenUsHeader>
+      <ListenAlbumsWrapper>
+        {cards.map((card) => {
+          return (
+            <Card name={card.name} coverSrc={card.cover} tags={card.data} />
+          );
+        })}
+      </ListenAlbumsWrapper>
+    </ListenUsSection>
+  );
+}
--- src/ui/ui.tsx
+++ src/ui/ui.tsx
@@ -1,98 +1,98 @@
-import styled from '@emotion/styled';
-import React from 'react';
-import { MainView } from './pages/MainView/MainView';
-import About from 'src/assets/images/about.jpg';
-import About2 from 'src/assets/images/about2.jpg';
-import { ListenUsView } from 'src/ui/pages/ListenUsView/ListenUsView';
-import { colors, fonts } from 'src/config/theme';
-import { YouTubeView } from 'src/ui/pages/YouTubeView/YouTubeView';
-import { ContactSection } from 'src/ui/pages/ContactSection/ContactSection';
+import styled from "@emotion/styled";
+import React from "react";
+import { MainView } from "./pages/MainView/MainView";
+import About from "src/assets/images/about.jpg";
+import About2 from "src/assets/images/about2.jpg";
+import { ListenUsView } from "src/ui/pages/ListenUsView/ListenUsView";
+import { colors, fonts } from "src/config/theme";
+import { YouTubeView } from "src/ui/pages/YouTubeView/YouTubeView";
+import { ContactSection } from "src/ui/pages/ContactSection/ContactSection";
 interface SectionWrapperPropsType {
-    imageUrl: string
+  imageUrl: string;
 }
 const breakpoints = [576, 768, 992, 1200];
 
-const mq = breakpoints.map(
-    bp => `@media (max-width: ${bp}px)`
-);
-export const SectionWrapper = styled('section') <SectionWrapperPropsType>`
-    display:flex;
-    align-items: end;
-    justify-content: end;
-    height: 100vh;
-    width: 100%;
-    background: ${({ imageUrl }): string => `url('${imageUrl}')`};
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
-    background-position: right;
-    overflow: scroll;
-  `;
-
-export const TextWrapper = styled('div')`
-    display: flex;
-    align-items: center;
-    background-color: #0000009e;
-    border-radius: 1.5em;
-    width: 100%;
-    height: auto;
-    margin: 0 15% 10%;
-    padding: 10px;
-    ${mq[1]}{
-        margin: 0px 15px 60px;
-    }
+const mq = breakpoints.map((bp) => `@media (max-width: ${bp}px)`);
+export const SectionWrapper = styled("section")<SectionWrapperPropsType>`
+  display: flex;
+  align-items: end;
+  justify-content: end;
+  height: 100vh;
+  width: 100%;
+  background: ${({ imageUrl }): string => `url('${imageUrl}')`};
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: right;
+  /* overflow: scroll; */
 `;
 
-export const TextMedium = styled('p')`
-    color: ${colors.neutrals.N10};
-    font-family: ${fonts.mulish.Regular};
-    font-size: 1.2em;
-    line-height: 2em;
-    width: 100%;
-    letter-spacing: 1.5px;
-    text-align: center;
-    ${mq[1]}{
-        text-align: initial;
-        padding: 10px;
-    }
-  `;
-export const ListWrapper = styled('ul')`
-    text-align: initial;
-  `;
+export const TextWrapper = styled("div")`
+  display: flex;
+  align-items: center;
+  background-color: #0000009e;
+  border-radius: 1.5em;
+  width: 100%;
+  max-height: 100vh;
+  /* overflow: scroll; */
+  margin: 5% 15% 10%;
+  padding: 10px;
+  ${mq[1]} {
+    margin: 0px 15px 60px;
+  }
+`;
 
+export const TextMedium = styled("p")`
+  color: ${colors.neutrals.N10};
+  font-family: ${fonts.mulish.Regular};
+  font-size: 1.2em;
+  line-height: 2em;
+  width: 100%;
+  letter-spacing: 1.5px;
+  text-align: center;
+  ${mq[1]} {
+    text-align: initial;
+    padding: 10px;
+  }
+`;
+export const ListWrapper = styled("ul")`
+  text-align: initial;
+`;
 
 export function Ui() {
-    return (
-        <>
-            <MainView />
-            <SectionWrapper imageUrl={About}>
-                <TextWrapper>
-                    <TextMedium>
-                        Jesteśmy młodą i dynamiczną grupą, która kocha muzykę i kulturę swingową. Nasz zespół powstał w Krakowie,
-                        gdzie w 2016 roku zaczęliśmy naszą przygodę od małych swingowych potańcówek w Piwnicy pod Baranami.
-                    </TextMedium>
-                </TextWrapper>
-            </SectionWrapper >
-            <ListenUsView />
-            <SectionWrapper imageUrl={About2}>
-                <TextWrapper>
-                    <TextMedium>
-                        Nasza pasja do muzyki swingowej prowadzi nas nie tylko po całej Polsce,
-                        ale także poza jej granice. <br />
-                        Zawitaliśmy między innymi na takie festiwale jak:
-                        <ListWrapper>
-                            <li>Dragon Swing Festival,</li>
-                            <li>Transvingvania Lindy Exchange,</li>
-                            <li>Letni Festiwal Jazzowy w Krakowie,</li>
-                            <li>Lindy Hop Non Stop,</li>
-                            <li>Kraków Lindy Invasion</li>
-                            <br />i wiele innych...
-                        </ListWrapper>
-                    </TextMedium>
-                </TextWrapper>
-            </SectionWrapper>
-            <YouTubeView />
-            <ContactSection />
-        </>
-    );
-};
+  return (
+    <>
+      <MainView />
+      <SectionWrapper imageUrl={About}>
+        <TextWrapper>
+          <TextMedium>
+            Jesteśmy młodą i dynamiczną grupą, która kocha muzykę i kulturę
+            swingową. Nasz zespół powstał w Krakowie, gdzie w 2016 roku
+            zaczęliśmy naszą przygodę od małych swingowych potańcówek w Piwnicy
+            pod Baranami.
+          </TextMedium>
+        </TextWrapper>
+      </SectionWrapper>
+      <ListenUsView />
+      <SectionWrapper imageUrl={About2}>
+        <TextWrapper>
+          <TextMedium>
+            Nasza pasja do muzyki swingowej prowadzi nas nie tylko po całej
+            Polsce, ale także poza jej granice. <br />
+            Zawitaliśmy między innymi na takie festiwale jak:
+            <ListWrapper>
+              <li>Dragon Swing Festival,</li>
+              <li>Transvingvania Lindy Exchange,</li>
+              <li>Letni Festiwal Jazzowy w Krakowie,</li>
+              <li>Lindy Hop Non Stop,</li>
+              <li>Kraków Lindy Invasion</li>
+              <br />i wiele innych...
+            </ListWrapper>
+          </TextMedium>
+        </TextWrapper>
+      </SectionWrapper>
+      <YouTubeView />
+      <ContactSection />
+    </>
+  );
+}
