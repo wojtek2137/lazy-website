e4aa7da1521479dd86837c3fa5cc68e1af86ca9e
wojtek2137 <adebe.beats@gmail.com>
2026-06-10 18:43:51 +0200

    Updates

--- _next/static/GC4TCxYWBPId1vb9NrQK9/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-5e59d1a7d44227b4.js"],"/404":["static/chunks/pages/404-25337dc197f3bcd4.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
